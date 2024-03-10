2821a9c8be1fa37584ce14444994099dd747ffcf
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-03-09 23:06:38 -0300

    Componente Input

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import { StatusBar, View } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
+
 import { Loading } from '@components/Loading';
+import {THEME} from './src/theme';
+import { SingIn } from '@screens/Singin';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -10,13 +13,13 @@ export default function App() {
   })
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME} >
       <StatusBar
         barStyle="light-content"
         backgroundColor="#202024"
         translucent
       />
-      <Loading />
+     {fontsLoaded ? <SingIn /> : <Loading />}
     </NativeBaseProvider>
   )
 }
