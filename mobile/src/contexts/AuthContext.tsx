3978490e70b87419593415aa2d73a117e5cacfcd
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-03-18 08:42:43 -0300

    Compartilhando estados com as informações

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { UserDTO } from "@dtos/UserDTO";
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export type AuthContentDataProps = {
     user: UserDTO;
@@ -12,15 +12,14 @@ type AuthContentProviderProps = {
 export const AuthContext = createContext<AuthContentDataProps>({} as AuthContentDataProps)
 
 export function AuthContextProvider({ children }: AuthContentProviderProps) {
+    const [user, setUser] = useState({
+        id: '1',
+        name: 'Marcos Alexandre',
+        email: 'marcos@gmail.com',
+        avatar: 'marcos.png'
+    })
     return (
-        <AuthContext.Provider value={{
-            user: {
-                id: '1',
-                name: 'Marcos Alexandre',
-                email: 'marcos@gmail.com',
-                avatar: 'marcos.png'
-            }
-        }}>
+        <AuthContext.Provider value={{ user: user }}>
             {children}
         </AuthContext.Provider>
     )
