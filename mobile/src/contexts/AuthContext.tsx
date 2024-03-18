0d2cda543f2263a74f7683d279aacb6dd52b5b1a
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-03-18 08:20:05 -0300

    Criação e compartilhamento do contexto para aplicação

---
+++ mobile/src/contexts/AuthContext.tsx
@@ -0,0 +1,27 @@
+import { UserDTO } from "@dtos/UserDTO";
+import { createContext } from "react";
+
+export type AuthContentDataProps = {
+    user: UserDTO;
+}
+
+type AuthContentProviderProps = {
+    children: React.ReactNode;
+}
+
+export const AuthContext = createContext<AuthContentDataProps>({} as AuthContentDataProps)
+
+export function AuthContextProvider({ children }: AuthContentProviderProps) {
+    return (
+        <AuthContext.Provider value={{
+            user: {
+                id: '1',
+                name: 'Marcos',
+                email: 'marcos@gmail.com',
+                avatar: 'marcos.png'
+            }
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -3,10 +3,17 @@ import { useTheme, Box } from "native-base";
 
 import { AppRoutes } from "./app.routes";
 import { AuthRouthes } from "./auth.routes";
+import { AuthContext } from "@contexts/AuthContext";
+import { useContext } from "react";
 
 
 export function Routes(){
     const { colors } = useTheme()
+    
+    const contextData = useContext( AuthContext )
+    console.log(contextData)
+
+
     const theme = DefaultTheme
     theme.colors.background = colors.gray[700]
 
