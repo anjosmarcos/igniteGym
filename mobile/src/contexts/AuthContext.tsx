7b5c5b181cadb66a47c6fbb603e00e1b84670167
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-03-19 09:19:58 -0300

    Salvando os dados login no storage do dispositivo usando async storage

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -2,6 +2,8 @@ import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
 import { createContext, useState } from "react";
 
+import { storageUserSave } from "@storage/storageUser"
+
 export type AuthContentDataProps = {
     user: UserDTO;
     signIn: (email: string, password: string) => Promise<void>;
@@ -22,6 +24,7 @@ export function AuthContextProvider({ children }: AuthContentProviderProps) {
 
             if (data.user) {
                 setUser(data.user)
+                storageUserSave(data.user)
             }
         }
         catch (error) {
